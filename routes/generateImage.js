203fe19ff855f64b8082f36fc8c907effc3b0826
usakowhity <usakowhity@yahoo.co.jp>
2026-01-26 22:06:07 +0900

    generateImage.js, vercel.json修正

--- routes/generateImage.js
+++ routes/generateImage.js
@@ -9,7 +9,7 @@ const router = express.Router();
 // Supabase
 const supabase = createClient(
   process.env.SUPABASE_URL,
-  process.env.SUPABASE_SERVICE_KEY
+  process.env.SUPABASE_ANON_KEY
 );
 
 // POST /generate-image
